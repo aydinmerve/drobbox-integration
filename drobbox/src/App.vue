c3c19d5686044677849ee6d4473958b31b142233
aydinmerve <merve9328@gmail.com>
2021-06-18 16:38:03 +0300

    Vue drobbox integration

---
+++ drobbox/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="auto" src="./assets/Vue-logo.png">
+    <DropboxSelector/>
+  </div>
+</template>
+
+<script>
+import DropboxSelector from "./components/DropboxSelector";
+
+export default {
+  name: "App",
+  components: {
+    DropboxSelector
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
